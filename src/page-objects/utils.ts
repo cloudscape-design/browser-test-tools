05a76bec233088b6f7fce3ee186da8a670a13c0d
Florian Dreschner <fdresch@amazon.com>
2022-09-04 06:40:48 +0000

    feat: Support iOS > 14
    Support iOS > 14 by cropping top and botto of the screen.

--- src/page-objects/utils.ts
+++ src/page-objects/utils.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ViewportSize, ElementRect } from './types';
+import { ElementRect } from './types';
 
 export function getElementCenter(rect: ElementRect): { x: number; y: number } {
   return {
@@ -9,72 +9,22 @@ export function getElementCenter(rect: ElementRect): { x: number; y: number } {
   };
 }
 
-// This is the pixel value *before* applying the device pixel ratio
-const iosAddressBarHeight = 50;
-
-// Device specific sizes for iOS devices. Models before iPhone X are covered by the default value.
-// Viewport values taken from https://yesviz.com/iphones.php
-const iosDeviceConfig = [
-  {
-    name: 'iPhone 12 (Pro)',
-    width: 390,
-    height: 844,
-    statusBarHeight: 141,
-  },
-  {
-    name: 'iPhone 12 Pro Max',
-    width: 428,
-    height: 926,
-    statusBarHeight: 141,
-  },
-  {
-    name: 'iPhone 11 Pro',
-    width: 375,
-    height: 812,
-    statusBarHeight: 132,
-  },
-  {
-    name: 'iPhone 11 Pro Max',
-    width: 414,
-    height: 896,
-    statusBarHeight: 132,
-  },
-];
-
-// Screenshots on iOS include all iOS UI elements like toolbars and navigation, so we need to
-// cut them out of screenshots. This function provides relevant offsets.
-export function calculateIosTopOffset(dimensions: Pick<ViewportSize, 'screenWidth' | 'screenHeight'>): number {
-  const { screenWidth: width, screenHeight: height } = dimensions;
-
-  // Default height for most models before the iPhone X
-  let statusBarHeight = 20;
-
-  const deviceConfig = iosDeviceConfig.find(
-    config =>
-      (width === config.width && height === config.height) || (width === config.height && height === config.width)
-  );
-  if (deviceConfig) {
-    statusBarHeight = deviceConfig.statusBarHeight;
-  }
-
-  const addressBarHeight = iosAddressBarHeight;
-
-  return statusBarHeight + addressBarHeight;
-}
+// This is the pixel values *before* applying the device pixel ratio
+const iosStatusBar = 47;
+const iosTabBar = 133;
 
 interface Dimensions {
   width: number;
   height: number;
 }
 
-export function getIosDeviceMask(image: Dimensions, screen: Dimensions): ElementRect {
-  const offsetTop = calculateIosTopOffset({ screenWidth: screen.width, screenHeight: screen.height });
+export function getIosDeviceMask(image: Dimensions): ElementRect {
   return {
-    top: offsetTop,
+    top: iosStatusBar,
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: iosTabBar,
     width: image.width,
-    height: image.height - offsetTop,
+    height: image.height - iosStatusBar - iosTabBar,
   };
 }
