cf9bd92fc6246aa4b152f30f1af29ef2bd2124f4
Timo Gasda <gasda@amazon.com>
2022-11-24 15:17:32 +0100

    Fix mocks

--- test/browsers/devicefarm-mobile.test.js
+++ test/browsers/devicefarm-mobile.test.js
@@ -2,31 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 const MobileBrowserCreator = require('../../src/browsers/devicefarm-mobile').default;
 
+const seleniumUrl = 'http://localhost:4444/wd';
+
 describe('Mobile Devicefarm browserCreator', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   test('can pass down selenium URL', async () => {
-    const browserCreator = new MobileBrowserCreator('iOS', {
-      seleniumUrl: 'http://localhost:1234/path',
-    });
+    const browserCreator = new MobileBrowserCreator('iOS', { seleniumUrl });
 
-    const { hostname, path } = await browserCreator.getBrowser({});
-
+    const { hostname } = await browserCreator.__getBrowserUrl();
     expect(hostname).toBe('localhost');
-    expect(path).toBe('/path');
   });
 
-  test('can pass down capabilities', async () => {
-    const browserCreator = new MobileBrowserCreator('Android', {
-      seleniumUrl: 'http://localhost:4444',
-    });
+  test.each([{ platform: 'Android' }, { platform: 'iOS' }])('adds $platform capabilities', () => {
+    const browserCreator = new MobileBrowserCreator('Android', { seleniumUrl });
 
-    const { logLevel } = await browserCreator.getBrowser({
-      logLevel: 'trace',
-    });
-
-    expect(logLevel).toBe('trace');
+    const capabilities = browserCreator.__getCapabilities();
+    expect(capabilities['appium:platformName']).toBe('Android');
   });
 });
