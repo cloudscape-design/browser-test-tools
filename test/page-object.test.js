c26eb2d33edbdd8307805355cfb9b1af3454af78
Timo Gasda <gasda@amazon.com>
2023-04-13 07:51:34 +0200

    Add some tests

--- test/page-object.test.js
+++ test/page-object.test.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 const { ScreenshotPageObject } = require('../src/page-objects');
+const { calculateIosTopOffset } = require('../src/page-objects/utils');
 const useBrowser = require('../src/use-browser').default;
 
 function setupTest(testFn) {
@@ -291,3 +292,18 @@ test(
     await expect(page.getLiveAnnouncements()).resolves.toEqual([]);
   })
 );
+
+describe('calculateIosTopOffset', () => {
+  test('falls back to default offset if device cannot be identified', () => {
+    expect(calculateIosTopOffset({ screenWidth: 99, screenHeight: 99, pixelRatio: 1 })).toBe(20);
+    expect(calculateIosTopOffset({ screenWidth: 99, screenHeight: 99, pixelRatio: 2 })).toBe(40);
+  });
+
+  test('returns offsets for recognized devices', () => {
+    // iPhone 12 Pro
+    expect(calculateIosTopOffset({ screenWidth: 390, screenHeight: 844, pixelRatio: 2 })).toBe(141);
+
+    // iPhone 11 Pro
+    expect(calculateIosTopOffset({ screenWidth: 375, screenHeight: 812, pixelRatio: 2 })).toBe(132);
+  });
+});
