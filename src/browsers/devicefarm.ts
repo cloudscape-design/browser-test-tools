d1bb34ccc67d5a2088a08af0aaf96ede9b5a3eb7
Boris Serdiuk <serdiuk@amazon.com>
2023-09-06 13:58:22 +0200

    upgrade aws-sdk dependency

--- src/browsers/devicefarm.ts
+++ src/browsers/devicefarm.ts
@@ -1,10 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { URL } from 'url';
-import pRetry, { AbortError } from 'p-retry';
-import DeviceFarm from 'aws-sdk/clients/devicefarm';
-import { CredentialsOptions } from 'aws-sdk/lib/credentials';
-import { AWSError } from 'aws-sdk/lib/error';
+import { DeviceFarm } from '@aws-sdk/client-device-farm';
 import BrowserCreator from './browser-creator';
 import defaultCapabilities, { Capabilities, getCapability } from './capabilities';
 import { FatalError } from '../exceptions';
@@ -13,34 +10,17 @@ export interface DevicefarmOptions {
   retryCount?: number;
   projectArn: string;
   expiresInSeconds?: number;
-  credentials: CredentialsOptions;
 }
 
 export default class DevicefarmBrowserCreator extends BrowserCreator {
   protected async __getBrowserUrl(): Promise<URL> {
     const options = this.options as DevicefarmOptions;
-    const client = new DeviceFarm({ credentials: options.credentials, region: 'us-west-2' });
+    const client = new DeviceFarm({ region: 'us-west-2' });
 
-    const response = await pRetry(
-      async () => {
-        try {
-          const response = await client
-            .createTestGridUrl({
-              projectArn: options.projectArn,
-              expiresInSeconds: options.expiresInSeconds || 300,
-            })
-            .promise();
-          return response;
-        } catch (error) {
-          if (isAWSError(error) && error.code === 'ThrottlingException') {
-            // Retries with backoff when throttled
-            throw error;
-          }
-          throw new AbortError(error as Error); // Abort on any other error
-        }
-      },
-      { retries: options.retryCount || 10 }
-    );
+    const response = await client.createTestGridUrl({
+      projectArn: options.projectArn,
+      expiresInSeconds: options.expiresInSeconds || 300,
+    });
 
     if (!response.url) {
       throw new FatalError('Invalid response from devicefarm: ' + JSON.stringify(response));
@@ -51,8 +31,4 @@ export default class DevicefarmBrowserCreator extends BrowserCreator {
   protected __getCapabilities(): Capabilities {
     return getCapability(this.browserName, defaultCapabilities);
   }
-}
-
-function isAWSError(obj: unknown): obj is AWSError {
-  return obj instanceof Error && typeof (obj as { code?: string })['code'] === 'string';
 }
