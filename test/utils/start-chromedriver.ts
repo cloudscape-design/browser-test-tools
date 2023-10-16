9a7fb0b024ebde837e2cf95b1f2a2039e9f72676
Timo <gasda@amazon.com>
2023-10-16 16:06:01 +0200

    Update DNS resolving order

--- test/utils/start-chromedriver.ts
+++ test/utils/start-chromedriver.ts
@@ -1,8 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import dns from 'dns';
 import { startWebdriver } from '../../dist/chrome-launcher';
 import { chromeDriverPort } from './config';
 
 export default async () => {
+  dns.setDefaultResultOrder('ipv4first');
   await startWebdriver(chromeDriverPort);
 };
