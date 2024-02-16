815e02c0ebdd67eb157bce59329160c305ec67c6
Michael Dowse <dowsem@amazon.com>
2024-02-16 15:40:05 +0100

    chore: Refactor to move all artifacts to lib folder

--- test/utils/stop-chromedriver.ts
+++ test/utils/stop-chromedriver.ts
@@ -1,5 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { shutdownWebdriver } from '../../dist/chrome-launcher';
+import { shutdownWebdriver } from '../../lib/chrome-launcher';
 
 export default shutdownWebdriver;
