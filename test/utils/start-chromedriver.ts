16582ae9ea0ecd9b4b2e3583c74c1c069b36d8a0
Michael Dowse <dowsem@amazon.com>
2024-02-16 15:40:05 +0100

    chore: Refactor to move all artifacts to lib folder

--- test/utils/start-chromedriver.ts
+++ test/utils/start-chromedriver.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import dns from 'dns';
-import { startWebdriver } from '../../dist/chrome-launcher';
+import { startWebdriver } from '../../lib/chrome-launcher';
 import { chromeDriverPort } from './config';
 
 export default async () => {
