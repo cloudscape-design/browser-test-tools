6dedaf4def43c040cce35b82a0c658b991ed0c0f
Joan Perals Tresserra <jotresse@amazon.com>
2024-06-10 10:53:35 +0200

    More debugging

--- test/utils/global-setup.ts
+++ test/utils/global-setup.ts
@@ -3,8 +3,10 @@
 import dns from 'node:dns';
 import { startWebdriver, shutdownWebdriver } from '../../lib/chrome-launcher';
 import { chromeDriverPort } from './config';
+console.log('setup imported', performance.now());
 
 export async function setup() {
+  console.log('setup', performance.now());
   // Make sure to favor IPv4 name resolution over IPv6 so that the localhost debugger can be found.
   dns.setDefaultResultOrder('ipv4first');
   console.log('startWebdriver', performance.now());
