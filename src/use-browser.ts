c237f9ef9db46c651c2a289faca6d678941e3feb
Joan Perals Tresserra <jotresse@amazon.com>
2024-06-20 11:14:41 +0200

    Move memory logging to a more useful place

--- src/use-browser.ts
+++ src/use-browser.ts
@@ -30,10 +30,10 @@ function useBrowser(...args: [Partial<WebDriverOptions>, TestFunction] | [TestFu
   // How to do type-safe function overloads: https://stackoverflow.com/questions/55852612/typescript-overloads-optional-arguments-and-type-inference
   const optionsOverride = args.length === 1 ? {} : args[0];
   const testFn = args.length === 1 ? args[0] : args[1];
-  console.log('Free memory:', os.freemem());
-  console.log('Total memory:', os.totalmem());
-  console.log(process.memoryUsage());
   return async () => {
+    console.log('Free memory:', os.freemem());
+    console.log('Total memory:', os.totalmem());
+    console.log(process.memoryUsage());
     const creator = getBrowserCreator(options.browserName, options.seleniumType, options.browserCreatorOptions);
     const browser = await creator.getBrowser({ ...options.webdriverOptions, ...optionsOverride });
     try {
