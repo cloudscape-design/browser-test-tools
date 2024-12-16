01c74db1ff02a8b3b97b42f6391f13e99f9c93f7
Joan Perals Tresserra <jotresse@amazon.com>
2024-12-16 20:07:36 +0100

    Drop Puppeteer strategy for screenshots

--- src/page-objects/full-page-screenshot.ts
+++ src/page-objects/full-page-screenshot.ts
@@ -52,12 +52,6 @@ export async function scrollAndMergeStrategy(browser: WebdriverIO.Browser) {
 }
 
 export default async function fullPageScreenshot(browser: WebdriverIO.Browser, forceScrollAndMerge: boolean = false) {
-  const puppeteer = await getPuppeteer(browser);
-  if (puppeteer && !forceScrollAndMerge) {
-    // casting due to mismatch in NodeJS types of EventEmitter
-    return puppeteerStrategy(browser, puppeteer as unknown as PuppeteerBrowser);
-  }
-  console.warn('Falling back to scroll-and-merge strategy');
   return scrollAndMergeStrategy(browser);
 }
 
