1e7ae876ee6291c2161dc2986fdce7013e0d1d10
Joan Perals Tresserra <jotresse@amazon.com>
2024-12-18 14:00:05 +0100

    Reapply "revert: "chore: Show scrollbars in Chrome-based integration tests (#120)" (#122)"
    This reverts commit dc7dbdf1b2d114acee0f1ebc4f8c1942592ea0a0.

--- src/browsers/capabilities.ts
+++ src/browsers/capabilities.ts
@@ -43,6 +43,7 @@ const defaultCapabilities: Record<string, Capabilities> = {
         '--enable-simple-cache-backend',
         '--enable-tcp-fast-open',
         '--enable-webgl',
+        '--hide-scrollbars',
         '--ignore-gpu-blacklist',
         '--media-cache-size=33554432',
         '--metrics-recording-only',
