2c5859ebdd0d5df1c2fe0766a7d2e5b568f46959
Joan Perals <jperals@protonmail.com>
2025-03-03 08:11:08 +0100

    Revert "chore: Upgrade Safari to version 16 (#135)"
    This reverts commit 8281be6109297cb2749ccb01ebd39b91cbb50d3d.

--- src/browsers/browserstack.ts
+++ src/browsers/browserstack.ts
@@ -33,11 +33,11 @@ const browsers: Record<string, WebdriverIO.Capabilities> = {
   },
   Safari: {
     browserName: 'safari',
-    browserVersion: '16.5',
+    browserVersion: '15.3',
     'bstack:options': {
       seleniumVersion: '3.141.59',
       os: 'OS X',
-      osVersion: 'Ventura',
+      osVersion: 'Monterey',
     },
   },
   IE11: {
