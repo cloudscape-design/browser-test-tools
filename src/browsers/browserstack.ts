5141eed440c6bfb751baa7908db3ac05e51fd45c
Johannes Weber <jowejowe@amazon.com>
2025-10-27 14:09:54 +0100

    chore: Upgrade Safari to 18.4
    We do support the latest three minor versions of Safari. With that change, we do test on a newer version of Safari (18.4) - 18.5 is not available on Browserstack.
    
    Latest Safari minor Versions (https://developer.apple.com/documentation/safari-release-notes):
     - 26.0 (latest)
     - 18.6
     - 18.5
     - 18.4 (this)

--- src/browsers/browserstack.ts
+++ src/browsers/browserstack.ts
@@ -33,11 +33,11 @@ const browsers: Record<string, WebdriverIO.Capabilities> = {
   },
   Safari: {
     browserName: 'safari',
-    browserVersion: '16.5',
+    browserVersion: '18.4',
     'bstack:options': {
       seleniumVersion: '3.141.59',
       os: 'OS X',
-      osVersion: 'Ventura',
+      osVersion: 'Sequoia',
     },
   },
   IE11: {
