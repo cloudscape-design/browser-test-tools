7a35db5d0927bac3a11600757c1a3cce63ce3a5c
Joan Perals Tresserra <jotresse@amazon.com>
2026-06-24 13:37:19 +0200

    Type fixes

--- src/page-objects/index.ts
+++ src/page-objects/index.ts
@@ -1,11 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 export { default as BasePageObject } from './base';
-export {
-  default as ScreenshotPageObject,
-  PermutationScreenshot,
-  RawPermutationScreenshot,
-  DecodedPermutationScreenshot,
-} from './screenshot';
+export { default as ScreenshotPageObject, PermutationScreenshot, RawPermutationScreenshot } from './screenshot';
 export { default as EventsSpy } from './events-spy';
 export { Screenshot, RawScreenshot, ScreenshotWithOffset, ElementSize, ElementRect, ElementOffset } from './types';
