33d66b9c4037cc6eedfb898b57e2e10226882113
Joan Perals Tresserra <jotresse@amazon.com>
2026-07-31 07:59:38 +0200

    Fix test

--- test/base-page-object.test.ts
+++ test/base-page-object.test.ts
@@ -304,7 +304,7 @@ test(
 test(
   'doubleClick',
   setupTest(async page => {
-    await page.doubleClick('#dblclick-button');
+    await page.doubleClick('#button');
     await page.waitForVisible('#dblclick-message');
     expect(await page.getText('#dblclick-message')).toEqual('Double-clicked!');
   })
